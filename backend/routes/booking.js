596d9ad13b70136a8820c6e8afd4ebfa65d0b081
Sanu334 <maadarabuu132@gmail.com>
2024-05-25 22:06:53 +0530

    api to book and car updated

--- backend/routes/booking.js
+++ backend/routes/booking.js
@@ -20,15 +20,20 @@ booking.post("/", async (req, res) => {
         const end = body.maxDate;
 
         const overlappingBooking = await Booking.findOne({
+          wheels: body.wheels,
           vehicle: body.vehicle,
           $or: [{ minDate: { $lte: end }, maxDate: { $gte: start } }],
         });
 
         if (overlappingBooking) {
-          return res
-            .status(400)
-            .send("The vehicle is already booked for the selected dates.");
+          return res.status(400).send({
+            message: "The vehicle is not available for the selected dates",
+          });
         }
+
+        const isBikeAvailable = await Bike.findOne({ vehicle: body.vehicle });
+        if (!isBikeAvailable)
+          return res.send({ message: "sorry this Bike is not available" });
 
         const bookVehicles = new Booking({
           wheels: body.wheels,
@@ -39,23 +44,46 @@ booking.post("/", async (req, res) => {
 
         const data = await bookVehicles.save();
 
-        const obj = await Bike.findOne({
-          vehicle: body.vehicle,
-        });
-
-        obj.minDate = body.minDate;
-        obj.maxDate = body.maxDate;
-        obj.booked = true;
-        const result = await obj.save();
-        console.log("bike data updated");
+        console.log("bike booked");
         res.send({ message: "vehicle " + data.vehicle + " booked " });
         break;
 
       case "4":
+        const star = body.minDate;
+        const en = body.maxDate;
+
+        const overlappingBookin = await Booking.findOne({
+          wheels: body.wheels,
+          vehicle: body.vehicle,
+          $or: [{ minDate: { $lte: en }, maxDate: { $gte: star } }],
+        });
+
+        if (overlappingBookin) {
+          return res.status(400).send({
+            message: "The vehicle is not available for the selected dates.",
+          });
+        }
+
+        const isCarAvailable = await Car.findOne({ vehicle: body.vehicle });
+        if (!isCarAvailable)
+          return res.send({ message: "sorry this car is not available" });
+
+        const bookVehicle = new Booking({
+          wheels: body.wheels,
+          vehicle: body.vehicle,
+          minDate: body.minDate,
+          maxDate: body.maxDate,
+        });
+
+        const data2 = await bookVehicle.save();
+
+        res.send({ message: "vehicle " + data2.vehicle + " booked " });
         break;
     }
   } catch (error) {
-    res.status(400).send({ message: "could not be booked : " + error.message });
+    res
+      .status(400)
+      .send({ message: "4 wheeler could not be booked : " + error.message });
   }
 });
 
